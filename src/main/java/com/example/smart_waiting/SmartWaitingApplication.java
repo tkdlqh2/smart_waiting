38fc0fb37a89f1240016a8cbc7ad183c0497302a
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-03 01:35:26 +0900

    Initial commit

---
+++ src/main/java/com/example/smart_waiting/SmartWaitingApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_waiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWaitingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartWaitingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smart_waiting/SmartWaitingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_waiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartWaitingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

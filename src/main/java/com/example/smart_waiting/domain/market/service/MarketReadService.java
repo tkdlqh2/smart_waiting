c9f0e31e7d9c45bd4bab76debb75fa240bb3f73e
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-03 18:08:10 +0900

    feat : MarketService,UserService read/write 분리

--- src/main/java/com/example/smart_waiting/domain/market/service/MarketReadService.java
+++ src/main/java/com/example/smart_waiting/domain/market/service/MarketReadService.java
@@ -3,5 +3,5 @@ package com.example.smart_waiting.domain.market.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MarketService {
+public class MarketReadService {
 }
--- src/main/java/com/example/smart_waiting/domain/user/service/UserReadService.java
+++ src/main/java/com/example/smart_waiting/domain/user/service/UserReadService.java
@@ -3,5 +3,5 @@ package com.example.smart_waiting.domain.user.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserReadService {
 }

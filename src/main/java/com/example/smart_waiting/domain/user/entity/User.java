cb042162f8341016e7eb16d6c48bd4d1dd74e605
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-03 17:47:30 +0900

    feat : User entity 생성

---
+++ src/main/java/com/example/smart_waiting/domain/user/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.smart_waiting.domain.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "email", nullable = false,unique = true)
+    private String email;
+    private String password;
+
+    @Column(name = "phone", nullable = false)
+    private String name;
+    @Column(name = "phone", nullable = false,unique = true)
+    private String phone;
+}

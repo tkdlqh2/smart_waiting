ae26fec024f2e315f16d662577117ccb72f53146
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-04 02:02:17 +0900

    feat : sql 에 market과 constraint 추가

--- src/main/java/com/example/smart_waiting/schema/ddl.sql
+++ src/main/java/com/example/smart_waiting/schema/ddl.sql
@@ -1,10 +1,35 @@
 create table User
 (
-    id int auto_increment,
+    user_id int auto_increment,
     email varchar(20) not null,
     password varchar(20) not null,
     name varchar(10) not null,
     phone varchar(14) not null,
-    constraint member_id_uindex
+    constraint user_id_uindex
+        primary key (user_id)
+);
+
+create table Market
+(
+    id int auto_increment,
+    owner int not null,
+    name varchar(20) not null,
+    registrationNum varchar(10) not null,
+    zipCode int not null,
+    detail_address varchar(20) not null,
+    open_hour int not null,
+    close_hour int not null,
+    constraint market_id_uindex
         primary key (id)
 );
+
+ALTER TABLE `Market`
+ADD INDEX `ownerKey_idx` (`owner` ASC) VISIBLE;
+;
+
+ALTER TABLE `Market`
+ADD CONSTRAINT `ownerKey`
+  FOREIGN KEY (`owner`)
+  REFERENCES `User` (`user_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;

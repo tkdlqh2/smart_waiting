3eff617813abb5708d34cccae95364666a07589d
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-04 16:31:00 +0900

    test : createUser success test 완성

--- src/test/java/com/example/smart_waiting/domain/user/service/UserWriteServiceTest.java
+++ src/test/java/com/example/smart_waiting/domain/user/service/UserWriteServiceTest.java
@@ -1,18 +1,29 @@
 package com.example.smart_waiting.domain.user.service;
 
+import com.example.smart_waiting.domain.user.dto.UserInput;
+import com.example.smart_waiting.domain.user.entity.User;
+import com.example.smart_waiting.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
-void class UserWriteServiceTest {
+class UserWriteServiceTest {
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private UserRepository userRepository;
 
     @InjectMocks
     private UserWriteService userWriteService;
@@ -35,5 +46,10 @@ void class UserWriteServiceTest {
 
         //then
         verify(userRepository,times(1)).save(captor.capture());
+        var capturedUser = captor.getValue();
+        assertEquals("yhj7124@naver.com",capturedUser.getEmail());
+        assertEquals("2222",capturedUser.getPassword());
+        assertEquals("유형진",capturedUser.getName());
+        assertEquals("010-1111-2222",capturedUser.getPhone());
     }
 }

0e09dc9cd962e8b590d0dc1bbcebb1f2f00cdc4c
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-04 16:24:19 +0900

    feat : UserRepository class 추가

---
+++ src/main/java/com/example/smart_waiting/domain/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.smart_waiting.domain.user.repository;
+
+import com.example.smart_waiting.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}

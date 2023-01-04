e8a7b2468a3aa068798a02ac045878c691efb057
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-04 16:39:22 +0900

    feat : UserInput 에 validation 추가

--- src/main/java/com/example/smart_waiting/domain/user/dto/UserInput.java
+++ src/main/java/com/example/smart_waiting/domain/user/dto/UserInput.java
@@ -3,14 +3,24 @@ package com.example.smart_waiting.domain.user.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class UserInput {
 
+    @Email
     private String email;
+    @NotBlank
+    @Size(min = 2)
     private String name;
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$")
     private String password;
+    @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$")
     private String phone;
 }

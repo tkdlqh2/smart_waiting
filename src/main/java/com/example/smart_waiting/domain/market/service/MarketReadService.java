dabbd9d0ece9f4581e6bb79b8809822ec890ba87
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-04 15:43:33 +0900

    feat : getWaitingInfo method 추가

--- src/main/java/com/example/smart_waiting/domain/market/service/MarketReadService.java
+++ src/main/java/com/example/smart_waiting/domain/market/service/MarketReadService.java
@@ -1,7 +1,28 @@
 package com.example.smart_waiting.domain.market.service;
 
+import com.example.smart_waiting.domain.market.dto.MarketWaitingInfoDTO;
+import com.example.smart_waiting.domain.market.repository.MarketWaitingTeamsRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
+@RequiredArgsConstructor
 @Service
 public class MarketReadService {
+
+    private final MarketWaitingTeamsRepository marketWaitingTeamsRepository;
+
+
+    public MarketWaitingInfoDTO getWaitingInfo(Long marketId) {
+
+        Long waitingTeamsNum = marketWaitingTeamsRepository.getWaitingTeamsNum(marketId);
+        Long totalExpectedTime = marketWaitingTeamsRepository.getExpectedTimePerTeam(marketId)
+                *waitingTeamsNum;
+
+        return MarketWaitingInfoDTO.builder()
+                .waitingTeams(waitingTeamsNum).
+                expectedMinutes(totalExpectedTime).
+                build();
+    }
+
 }

709c5ccb955bd11b1df1434f89c82b66147732ba
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-05 17:35:21 +0900

    feat : UserReadService 에 existEmail 추가

--- src/main/java/com/example/smart_waiting/domain/user/service/UserReadService.java
+++ src/main/java/com/example/smart_waiting/domain/user/service/UserReadService.java
@@ -1,7 +1,14 @@
 package com.example.smart_waiting.domain.user.service;
 
+import com.example.smart_waiting.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class UserReadService {
+
+    private final UserRepository userRepository;
+
+    public boolean existEmail(String email) {return userRepository.existsByEmail(email);}
 }

f262d9a409492e6201fd179f5dea1482a464dfdc
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-05 15:54:57 +0900

    test : emailAlreadyExist 실패 케이스 추가

--- src/test/java/com/example/smart_waiting/domain/user/service/UserWriteServiceTest.java
+++ src/test/java/com/example/smart_waiting/domain/user/service/UserWriteServiceTest.java
@@ -3,6 +3,7 @@ package com.example.smart_waiting.domain.user.service;
 import com.example.smart_waiting.domain.user.dto.UserInput;
 import com.example.smart_waiting.domain.user.entity.User;
 import com.example.smart_waiting.domain.user.repository.UserRepository;
+import com.example.smart_waiting.exception.NoErrorException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -11,10 +12,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static com.example.smart_waiting.exception.UserErrorCode.EMAIL_ALREADY_EXIST;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserWriteServiceTest {
@@ -51,5 +53,28 @@ class UserWriteServiceTest {
         assertEquals("2222",capturedUser.getPassword());
         assertEquals("유형진",capturedUser.getName());
         assertEquals("010-1111-2222",capturedUser.getPhone());
+    }
+
+    @Test
+    void createUserFail_emailAlreadyExist(){
+        //given
+        UserInput userInput = UserInput.builder()
+                .email("yhj7124@naver.com")
+                .password("1111")
+                .name("유형진")
+                .phone("010-1111-2222")
+                .build();
+
+        given(passwordEncoder.encode("1111")).willReturn("2222");
+        given(userRepository.findByEmail("yhj7124@naver.com")).willReturn(true);
+
+        //when
+        //then
+        try{
+            userWriteService.createUser(userInput);
+            throw new NoErrorException();
+        } catch(Exception e){
+            assertEquals(EMAIL_ALREADY_EXIST.getMessage(),e.getMessage());
+        }
     }
 }

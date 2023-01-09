3c68a108228684d3b7187710e3f689dbe06ac225
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-09 18:07:25 +0900

    feat : SignIn method 추가

--- src/main/java/com/example/smart_waiting/domain/user/service/UserReadService.java
+++ src/main/java/com/example/smart_waiting/domain/user/service/UserReadService.java
@@ -1,19 +1,30 @@
 package com.example.smart_waiting.domain.user.service;
 
+import com.example.smart_waiting.domain.user.dto.UserLogInInput;
+import com.example.smart_waiting.domain.user.entity.User;
 import com.example.smart_waiting.domain.user.repository.UserRepository;
+import com.example.smart_waiting.exception.UserException;
+import com.example.smart_waiting.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.example.smart_waiting.exception.error_code.UserErrorCode.PASSWORD_NOT_MATCH;
+import static com.example.smart_waiting.exception.error_code.UserErrorCode.USER_NOT_FOUND;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserReadService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Transactional(readOnly = true)
     public boolean existEmail(String email) {return userRepository.existsByEmail(email);}
@@ -25,5 +36,17 @@ public class UserReadService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException(username+"을 찾을 수 없습니다."));
+    }
+
+    @Transactional(readOnly = true)
+    public String signIn(UserLogInInput parameter) throws UserException {
+        User user = userRepository.findByEmail(parameter.getEmail())
+                .orElseThrow(()-> new UserException(USER_NOT_FOUND));
+
+        if(!passwordEncoder.matches(parameter.getPassword(), user.getPassword())){
+            throw new UserException(PASSWORD_NOT_MATCH);
+        }
+
+        return jwtTokenProvider.createToken(String.valueOf(user.getUsername()),user.getRoles());
     }
 }
--- src/main/java/com/example/smart_waiting/exception/error_code/UserErrorCode.java
+++ src/main/java/com/example/smart_waiting/exception/error_code/UserErrorCode.java
@@ -12,7 +12,8 @@ public enum UserErrorCode {
     PHONE_ALREADY_EXIST("이미 존재하는 핸드폰 번호입니다.",HttpStatus.BAD_REQUEST),
     USER_NOT_FOUND("해당 유저를 찾을 수 없습니다.",HttpStatus.NOT_FOUND),
     CODE_ALREADY_EXPIRED("인증 만료일이 지났습니다. 재가입을 해주세요",HttpStatus.BAD_REQUEST),
-    CODE_MISMATCH("코드가 일치하지 않습니다.",HttpStatus.BAD_REQUEST);
+    CODE_MISMATCH("코드가 일치하지 않습니다.",HttpStatus.BAD_REQUEST),
+    PASSWORD_NOT_MATCH("비밀번호가 일치하지 않습니다.",HttpStatus.BAD_REQUEST);
     private final String message;
     private final HttpStatus httpStatus;
 }

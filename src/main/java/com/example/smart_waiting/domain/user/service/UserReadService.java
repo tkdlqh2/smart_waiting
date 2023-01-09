a9cd4b1072f137e1e2ebce795fa4d90cdd72a06a
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-09 14:33:31 +0900

    feat : UserReadService 에 UserDetailsService 추가

--- src/main/java/com/example/smart_waiting/domain/user/service/UserReadService.java
+++ src/main/java/com/example/smart_waiting/domain/user/service/UserReadService.java
@@ -2,13 +2,16 @@ package com.example.smart_waiting.domain.user.service;
 
 import com.example.smart_waiting.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
 @RequiredArgsConstructor
 @Service
-public class UserReadService {
+public class UserReadService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
@@ -17,4 +20,10 @@ public class UserReadService {
 
     @Transactional(readOnly = true)
     public boolean existPhone (String phone) {return userRepository.existsByPhone(phone);}
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username+"을 찾을 수 없습니다."));
+    }
 }

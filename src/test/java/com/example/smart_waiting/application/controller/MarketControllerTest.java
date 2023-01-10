b2ffcf63562e70cd42d9064aaa270cdb8d2c7e00
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-11 03:37:44 +0900

    test : register 에 대한 controller test 추가 및 MockBean 설정 추가

---
+++ src/test/java/com/example/smart_waiting/application/controller/MarketControllerTest.java
@@ -0,0 +1,58 @@
+package com.example.smart_waiting.application.controller;
+
+import com.example.smart_waiting.domain.market.dto.MarketInput;
+import com.example.smart_waiting.domain.market.service.MarketWriteService;
+import com.example.smart_waiting.exception.exception_class.MarketException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.example.smart_waiting.exception.error_code.MarketErrorCode.ALREADY_HAVE_MARKET;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MarketController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@MockBean(JpaMetamodelMappingContext.class)
+class MarketControllerTest {
+
+    @MockBean
+    private MarketWriteService marketWriteService;
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void registerSuccess() throws Exception {
+
+        doNothing().when(marketWriteService).register(any(),any());
+
+        mockMvc.perform(post("/api/v1/market/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                MarketInput.builder()
+                                        .name("맛있는 음식점 1")
+                                        .registrationNum("000-11-22222")
+                                        .zipCode(11111L)
+                                        .detailAddress("상세주소입니다")
+                                        .openHour(1L)
+                                        .closeHour(14L)
+                                        .build()
+                        )))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value("음식점 등록이 완료되었습니다."))
+                .andDo(print());
+    }
+}
--- src/test/java/com/example/smart_waiting/application/controller/UserControllerTest.java
+++ src/test/java/com/example/smart_waiting/application/controller/UserControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(UserController.class)
 @AutoConfigureMockMvc(addFilters = false)
+@MockBean(JpaMetamodelMappingContext.class)
 class UserControllerTest {
 
     @MockBean

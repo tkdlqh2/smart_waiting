72492f566a3632e2d6c1b514e754f87d21a019db
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-11 02:18:56 +0900

    feat : MarketInput에 validation 추가

--- src/main/java/com/example/smart_waiting/domain/market/dto/MarketInput.java
+++ src/main/java/com/example/smart_waiting/domain/market/dto/MarketInput.java
@@ -5,15 +5,27 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class MarketInput {
+    @NotBlank
     private String name;
+    @Pattern(regexp = "^\\d{3}-\\d{2}-\\d{5}$")
     private String registrationNum;
+    @Min(10000)
+    @Max(99999)
     private Long zipCode;
+    @NotBlank
+    @Size(max = 50)
     private String detailAddress;
+    @Min(0)
+    @Max(23)
     private Long openHour;
+    @Min(0)
+    @Max(23)
     private Long closeHour;
 }

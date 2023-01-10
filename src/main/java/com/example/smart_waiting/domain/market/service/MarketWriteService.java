d7d9f33cb4ea0f07f2e24a55475b814237fe9b25
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-11 03:03:29 +0900

    feat : market 존재 여부 확인 추가

--- src/main/java/com/example/smart_waiting/domain/market/service/MarketWriteService.java
+++ src/main/java/com/example/smart_waiting/domain/market/service/MarketWriteService.java
@@ -4,8 +4,11 @@ import com.example.smart_waiting.domain.market.dto.MarketInput;
 import com.example.smart_waiting.domain.market.entity.Market;
 import com.example.smart_waiting.domain.market.repository.MarketRepository;
 import com.example.smart_waiting.domain.user.entity.User;
+import com.example.smart_waiting.exception.exception_class.MarketException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.example.smart_waiting.exception.error_code.MarketErrorCode.ALREADY_HAVE_MARKET;
 
 @RequiredArgsConstructor
 @Service
@@ -13,6 +16,7 @@ public class MarketWriteService {
     private final MarketRepository marketRepository;
 
     public void register(User user, MarketInput parameter){
+        if(marketRepository.existsByOwner(user)){throw new MarketException(ALREADY_HAVE_MARKET);}
         Market market = new Market(user,parameter);
         marketRepository.save(market);
     }

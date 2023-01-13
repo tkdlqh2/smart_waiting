1d670dce9438c12b9ac18fc7ecb41a91c94cc460
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-13 17:37:14 +0900

    feat : ParkType 추가

--- src/main/java/com/example/smart_waiting/domain/market/type/ParkType.java
+++ src/main/java/com/example/smart_waiting/domain/market/type/ParkType.java
@@ -1,4 +1,14 @@
 package com.example.smart_waiting.domain.market.type;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ParkType {
+    VALET("발렛 주차가 가능합니다"),
+    HAS_PARKING_LOT("주차장이 있습니다"),
+    FORBIDDEN("별도의 주차장이 없습니다.");
+
+    private final String msg;
 }

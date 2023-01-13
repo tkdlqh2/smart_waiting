a9d0a305692beb739461b1fbabe36842b08b9ab5
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-13 17:39:12 +0900

    feat : MarketFilter Getter 추가

--- src/main/java/com/example/smart_waiting/domain/market/dto/MarketFilter.java
+++ src/main/java/com/example/smart_waiting/domain/market/dto/MarketFilter.java
@@ -3,10 +3,12 @@ package com.example.smart_waiting.domain.market.dto;
 import com.example.smart_waiting.domain.market.type.FoodType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor

40f6bf9e43e8292b30d4014fa52436f399273435
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-13 19:06:11 +0900

    feat : MarketReadService 에 getMarketsByFilter method 추가

--- src/main/java/com/example/smart_waiting/domain/market/service/MarketReadService.java
+++ src/main/java/com/example/smart_waiting/domain/market/service/MarketReadService.java
@@ -1,7 +1,21 @@
 package com.example.smart_waiting.domain.market.service;
 
+import com.example.smart_waiting.domain.market.dto.MarketFilter;
+import com.example.smart_waiting.domain.market.entity.Market;
+import com.example.smart_waiting.domain.market.repository.MarketRepository;
+import com.example.smart_waiting.util.CursorRequest;
+import com.example.smart_waiting.util.PageCursor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class MarketReadService {
+
+    private final MarketRepository marketRepository;
+
+    public PageCursor<Market> getMarketsByFilter(MarketFilter filter, CursorRequest request){
+
+        return marketRepository.findByFilter(filter,request);
+    }
 }

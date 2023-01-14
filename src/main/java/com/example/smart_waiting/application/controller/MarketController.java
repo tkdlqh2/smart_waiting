c7cf8f4bd856068708a31545367847c346a2082c
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-15 01:48:55 +0900

    feat : controller 에 method 추가

--- src/main/java/com/example/smart_waiting/application/controller/MarketController.java
+++ src/main/java/com/example/smart_waiting/application/controller/MarketController.java
@@ -1,14 +1,16 @@
 package com.example.smart_waiting.application.controller;
 
+import com.example.smart_waiting.domain.market.dto.MarketDto;
+import com.example.smart_waiting.domain.market.dto.MarketFilter;
 import com.example.smart_waiting.domain.market.dto.MarketInput;
+import com.example.smart_waiting.domain.market.service.MarketReadService;
 import com.example.smart_waiting.domain.market.service.MarketWriteService;
 import com.example.smart_waiting.domain.user.entity.User;
+import com.example.smart_waiting.util.CursorRequest;
+import com.example.smart_waiting.util.PageCursor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -19,6 +21,7 @@ import java.security.Principal;
 public class MarketController {
 
     private final MarketWriteService marketWriteService;
+    private final MarketReadService marketReadService;
 
     @PostMapping("/register")
     public ResponseEntity<String> register(Principal principal, @Valid @RequestBody MarketInput input){
@@ -26,4 +29,12 @@ public class MarketController {
         marketWriteService.register(user, input);
         return ResponseEntity.ok("음식점 등록이 완료되었습니다.");
     }
+
+    @GetMapping("/list")
+    public ResponseEntity<PageCursor<MarketDto>> getMarketsByFilter(@RequestBody MarketFilter filter,
+                                                                    @RequestBody CursorRequest request){
+
+        return ResponseEntity.ok(marketReadService.getMarketsByFilter(filter,request));
+    }
+
 }

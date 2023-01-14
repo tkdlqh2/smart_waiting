36897727f6fda2fffeaaa53fee5d24a675a44cf8
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-15 00:48:02 +0900

    feat : MarketDto 에 id 필드 추가

--- src/main/java/com/example/smart_waiting/domain/market/dto/MarketDto.java
+++ src/main/java/com/example/smart_waiting/domain/market/dto/MarketDto.java
@@ -1,11 +1,14 @@
 package com.example.smart_waiting.domain.market.dto;
 
 import com.example.smart_waiting.domain.market.type.FoodType;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Getter
 public class MarketDto {
 
+    private final Long id;
     private final String name;
     private final String rcate2;
     private final FoodType foodType;

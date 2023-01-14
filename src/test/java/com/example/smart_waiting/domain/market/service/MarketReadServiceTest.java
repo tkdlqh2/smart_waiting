2935c29d6d10a7973e05272c2932ad59377d80e3
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-15 00:50:09 +0900

    fix-test : getMarketsByFilterSuccess return 값을 MarketDto 로 변경

--- src/test/java/com/example/smart_waiting/domain/market/service/MarketReadServiceTest.java
+++ src/test/java/com/example/smart_waiting/domain/market/service/MarketReadServiceTest.java
@@ -1,7 +1,7 @@
 package com.example.smart_waiting.domain.market.service;
 
+import com.example.smart_waiting.domain.market.dto.MarketDto;
 import com.example.smart_waiting.domain.market.dto.MarketFilter;
-import com.example.smart_waiting.domain.market.entity.Market;
 import com.example.smart_waiting.domain.market.repository.MarketRepository;
 import com.example.smart_waiting.factory.MarketsFixtureFactory;
 import com.example.smart_waiting.util.CursorRequest;
@@ -15,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
@@ -38,7 +39,9 @@ class MarketReadServiceTest {
                                     .build();
 
         CursorRequest request= new CursorRequest(null,20);
-        List<Market> body = MarketsFixtureFactory.createLists(20);
+        List<MarketDto> body = MarketsFixtureFactory.createLists(20).stream()
+                .map(x-> new MarketDto(x.getId(),x.getName(),x.getRcate2(),x.getFoodType()))
+                .collect(Collectors.toList());
 
         given(marketRepository.findByFilter(marketFilter, request)).willReturn(
                 new PageCursor<>(request.next(10L),body));

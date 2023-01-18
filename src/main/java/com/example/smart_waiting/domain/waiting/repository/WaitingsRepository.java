39b858b475c16175fd0ce0536eb6db6f1b0b0335
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-19 06:45:25 +0900

    feat : WaitingsRepository 에 method 추가

--- src/main/java/com/example/smart_waiting/domain/waiting/repository/WaitingsRepository.java
+++ src/main/java/com/example/smart_waiting/domain/waiting/repository/WaitingsRepository.java
@@ -1,9 +1,16 @@
 package com.example.smart_waiting.domain.waiting.repository;
 
 import com.example.smart_waiting.domain.waiting.entity.Waitings;
+import com.example.smart_waiting.exception.error_code.WaitingsErrorCode;
+import com.example.smart_waiting.exception.exception_class.WaitingsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.ObjectUtils;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Repository
@@ -23,4 +30,22 @@ public class WaitingsRepository {
     public boolean existsByUserId(Long userId){
         return Boolean.TRUE.equals(redisTemplate.opsForSet().isMember(ALREADY_WAITING_USER_STRING, userId));
     }
+
+    public Waitings deleteByMarketId(Long marketId){
+        var userId = redisTemplate.opsForList().leftPop(MARKET_QUEUE_STRING+marketId);
+
+        if(!ObjectUtils.isEmpty(userId)){
+            redisTemplate.opsForSet().pop(ALREADY_WAITING_USER_STRING,userId);
+            return Waitings.builder()
+                    .marketId(marketId)
+                    .userId(userId)
+                    .build();
+        }
+
+        throw new WaitingsException(WaitingsErrorCode.MARKET_QUEUE_IS_ALREADY_EMPTY);
+    }
+
+    public List<Waitings> findAllByMarketId(Long marketId, int size){
+        return Objects.requireNonNull(redisTemplate.opsForList().range(MARKET_QUEUE_STRING + marketId, 0, size)).stream()
+                .map(x-> Waitings.builder().userId(x).marketId(marketId).build()).collect(Collectors.toList());}
 }

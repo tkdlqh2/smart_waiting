657eaf86b65376f83b65483bb88f77147ce20c80
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-19 06:20:56 +0900

    test : registerWaiting Fail case 추가

--- src/test/java/com/example/smart_waiting/domain/waiting/service/WaitingsWriteServiceTest.java
+++ src/test/java/com/example/smart_waiting/domain/waiting/service/WaitingsWriteServiceTest.java
@@ -3,6 +3,7 @@ package com.example.smart_waiting.domain.waiting.service;
 import com.example.smart_waiting.domain.user.entity.User;
 import com.example.smart_waiting.domain.waiting.entity.Waitings;
 import com.example.smart_waiting.domain.waiting.repository.WaitingsRepository;
+import com.example.smart_waiting.exception.NoErrorException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -10,11 +11,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static com.example.smart_waiting.exception.error_code.WaitingsErrorCode.ALREADY_REGISTERED_USER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -41,5 +39,20 @@ class WaitingsWriteServiceTest {
         var capturedWaitings = captor.getValue();
         assertEquals(user.getId(),capturedWaitings.getUserId());
         assertEquals(10L,capturedWaitings.getMarketId());
+    }
+
+    @Test
+    void registerWaitingsFail_alreadyRegisteredUser(){
+        //given
+        User user = User.builder().id(2L).build();
+        given(waitingsRepository.existsByUserId(user.getId())).willReturn(true);
+        //when
+        //then
+        try {
+            waitingsWriteService.registerWaiting(user, 10L);
+            throw new NoErrorException();
+        } catch (Exception e){
+            assertEquals(ALREADY_REGISTERED_USER.getMessage(),e.getMessage());
+        }
     }
 }

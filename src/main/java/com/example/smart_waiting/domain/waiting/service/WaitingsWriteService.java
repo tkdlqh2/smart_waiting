830c93d3338001f8b73e5f2ca12b14047e50febb
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-19 06:13:55 +0900

    feat : register User method 추가

---
+++ src/main/java/com/example/smart_waiting/domain/waiting/service/WaitingsWriteService.java
@@ -0,0 +1,30 @@
+package com.example.smart_waiting.domain.waiting.service;
+
+import com.example.smart_waiting.domain.user.entity.User;
+import com.example.smart_waiting.domain.waiting.entity.Waitings;
+import com.example.smart_waiting.domain.waiting.repository.WaitingsRepository;
+import com.example.smart_waiting.exception.exception_class.WaitingsException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.example.smart_waiting.exception.error_code.WaitingsErrorCode.ALREADY_REGISTERED_USER;
+
+@Service
+@RequiredArgsConstructor
+public class WaitingsWriteService {
+
+    private final WaitingsRepository waitingsRepository;
+
+    public void registerWaiting(User user, Long marketId){
+
+        if(waitingsRepository.existsByUserId(user.getId())){throw new WaitingsException(ALREADY_REGISTERED_USER);}
+
+        Waitings waitings = Waitings.builder()
+                .userId(user.getId())
+                .marketId(marketId)
+                .build();
+
+        waitingsRepository.save(waitings);
+    }
+
+}

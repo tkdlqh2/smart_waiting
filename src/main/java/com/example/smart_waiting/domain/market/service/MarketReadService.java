657610b7932d418b0cc376fef8209ccf44a14b99
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-20 03:23:37 +0900

    feat : getWaitingsResult 추가

--- src/main/java/com/example/smart_waiting/domain/market/service/MarketReadService.java
+++ src/main/java/com/example/smart_waiting/domain/market/service/MarketReadService.java
@@ -5,12 +5,14 @@ import com.example.smart_waiting.domain.market.dto.MarketDto;
 import com.example.smart_waiting.domain.market.dto.MarketFilter;
 import com.example.smart_waiting.domain.market.entity.Market;
 import com.example.smart_waiting.domain.market.repository.MarketRepository;
+import com.example.smart_waiting.domain.waiting.dto.WaitingsResult;
 import com.example.smart_waiting.exception.exception_class.MarketException;
 import com.example.smart_waiting.util.CursorRequest;
 import com.example.smart_waiting.util.PageCursor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
 
 import static com.example.smart_waiting.exception.error_code.MarketErrorCode.MARKET_NOT_FOUND;
 
@@ -20,6 +22,7 @@ import static com.example.smart_waiting.exception.error_code.MarketErrorCode.MAR
 public class MarketReadService {
 
     private final MarketRepository marketRepository;
+    private static final Long DEFAULT_WAITING_TIME_PER_TEAM = 5L;
 
     public PageCursor<MarketDto> getMarketsByFilter(MarketFilter filter, CursorRequest request){
 
@@ -32,5 +35,19 @@ public class MarketReadService {
                 .orElseThrow(()-> new MarketException(MARKET_NOT_FOUND));
 
         return MarketDetails.of(market);
+    }
+
+    public WaitingsResult getWaitingsResult(Long marketId, int priorTeams){
+
+        var market = marketRepository.findById(marketId)
+                .orElseThrow(()-> new MarketException(MARKET_NOT_FOUND));
+
+        Long waitingTimePerTeam = market.getWaitingTimePerTeam();
+        if(ObjectUtils.isEmpty(waitingTimePerTeam)){
+            waitingTimePerTeam = DEFAULT_WAITING_TIME_PER_TEAM;
+        }
+
+        return new WaitingsResult(priorTeams,
+                (priorTeams+1)*waitingTimePerTeam.intValue());
     }
 }

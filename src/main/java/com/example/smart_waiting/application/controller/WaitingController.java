9dca2cfbaa0f02812e3e1d2000bdb3f96895d805
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-20 05:07:16 +0900

    feat : User 가져오는 형식 변경
    Authentication 에서 getPrincipal로 가져옴

--- src/main/java/com/example/smart_waiting/application/controller/WaitingController.java
+++ src/main/java/com/example/smart_waiting/application/controller/WaitingController.java
@@ -5,12 +5,11 @@ import com.example.smart_waiting.domain.user.entity.User;
 import com.example.smart_waiting.domain.waiting.dto.WaitingsResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,8 +19,8 @@ public class WaitingController {
     private final RegisterWaitingsUsecase registerWaitingsUsecase;
 
     @PostMapping("/register/{marketId}")
-    public ResponseEntity<WaitingsResult> registerWaiting(Principal principal, @PathVariable Long marketId){
-        User user = (User) principal;
+    public ResponseEntity<WaitingsResult> registerWaiting(Authentication authentication, @PathVariable Long marketId){
+        User user = (User) authentication.getPrincipal();
         return ResponseEntity.ok(registerWaitingsUsecase.registerWaiting(user.getId(), marketId));
     }
 }

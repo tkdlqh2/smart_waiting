a7259be8239d999c5dcfd47c75bc708c72f809e0
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-20 03:02:08 +0900

    feat : Error Code 추가

--- src/main/java/com/example/smart_waiting/exception/error_code/WaitingsErrorCode.java
+++ src/main/java/com/example/smart_waiting/exception/error_code/WaitingsErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum WaitingsErrorCode {
 
-    ALREADY_REGISTERED_USER("이미 대기 신청한 식당이 존재합니다.",HttpStatus.BAD_REQUEST);
+    ALREADY_REGISTERED_USER("이미 대기 신청한 식당이 존재합니다.",HttpStatus.BAD_REQUEST),
+    MARKET_QUEUE_IS_ALREADY_EMPTY("큐에 남은 유저가 존재하지 않습니다",HttpStatus.BAD_REQUEST);
     private final String message;
     private final HttpStatus httpStatus;
 }

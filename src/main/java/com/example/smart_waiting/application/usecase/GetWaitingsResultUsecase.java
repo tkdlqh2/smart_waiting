145544ad94010e5e183c4496b2dafb33fc23ad33
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-01-21 04:22:37 +0900

    feat : usecase class 이름 변경 및 method 추가

--- src/main/java/com/example/smart_waiting/application/usecase/GetWaitingsResultUsecase.java
+++ src/main/java/com/example/smart_waiting/application/usecase/GetWaitingsResultUsecase.java
@@ -1,7 +1,10 @@
 package com.example.smart_waiting.application.usecase;
 
 import com.example.smart_waiting.domain.market.service.MarketReadService;
+import com.example.smart_waiting.domain.user.entity.User;
 import com.example.smart_waiting.domain.waiting.dto.WaitingsResult;
+import com.example.smart_waiting.domain.waiting.entity.Waitings;
+import com.example.smart_waiting.domain.waiting.service.WaitingsReadService;
 import com.example.smart_waiting.domain.waiting.service.WaitingsWriteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,13 +12,24 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class RegisterWaitingsUsecase {
+public class GetWaitingsResultUsecase {
     private final WaitingsWriteService waitingsWriteService;
+    private final WaitingsReadService waitingsReadService;
     private final MarketReadService marketReadService;
 
     @Transactional
     public WaitingsResult registerWaiting(Long userId, Long marketId){
         var priorTeams = waitingsWriteService.registerWaiting(userId, marketId);
         return marketReadService.getWaitingsResult(marketId,priorTeams);
+    }
+
+    @Transactional
+    public WaitingsResult getWaitings(Long userId){
+        var result = waitingsReadService.getCurrentWaiting(userId);
+        return marketReadService.getWaitingsResult(result.getMarketId(), result.getPriorTeams());
+    }
+
+    public Waitings handleWaiting(Long userId) {
+        return null;
     }
 }
